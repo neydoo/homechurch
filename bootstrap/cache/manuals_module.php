09e78c6768aa822312caf691a143f0e7d7d2bfb1
adedotunbashorun <adedotunolawale@gmail.com>
2020-03-17 21:08:14 +0100

    worked on homechurch users

---
+++ bootstrap/cache/manuals_module.php
@@ -0,0 +1,15 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\Manuals\\Providers\\ManualsServiceProvider',
+    1 => 'Modules\\Manuals\\Providers\\RouteServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\Manuals\\Providers\\ManualsServiceProvider',
+    1 => 'Modules\\Manuals\\Providers\\RouteServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
