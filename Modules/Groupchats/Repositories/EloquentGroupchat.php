346ff5bd33f6643cca2af2433bd8cb94c5b08b7f
adedotunbashorun <adedotunolawale@gmail.com>
2020-04-02 10:26:30 +0100

    worked on excel

--- Modules/Groupchats/Repositories/EloquentGroupchat.php
+++ Modules/Groupchats/Repositories/EloquentGroupchat.php
@@ -24,26 +24,26 @@ class EloquentGroupchat extends RepositoriesAbstract implements GroupchatInterfa
             return  getDataTabeleQuery($this->model)->get();
         }
         $query = getDataTabeleQuery($this->model)
-            ->join('churches', 'churches.id', '=', 'groupchats.church_id')
-            ->join('countries', 'countries.id', '=', 'groupchats.country_id')
-            ->join('regions', 'regions.id', '=', 'groupchats.region_id')
-            ->join('states', 'states.id', '=', 'groupchats.state_id')
-            ->join('districts', 'districts.id', '=', 'groupchats.district_id')
-            ->join('zones', 'zones.id', '=', 'groupchats.zone_id')
-            ->join('areas', 'areas.id', '=', 'groupchats.area_id')
+            // ->join('churches', 'churches.id', '=', 'groupchats.church_id')
+            // ->join('countries', 'countries.id', '=', 'groupchats.country_id')
+            // ->join('regions', 'regions.id', '=', 'groupchats.region_id')
+            // ->join('states', 'states.id', '=', 'groupchats.state_id')
+            // ->join('districts', 'districts.id', '=', 'groupchats.district_id')
+            // ->join('zones', 'zones.id', '=', 'groupchats.zone_id')
+            // ->join('areas', 'areas.id', '=', 'groupchats.area_id')
             ->select([
                 'groupchats.id as id',
                 'groupchats.name as name',
                 'groupchats.code as code',
                 'groupchats.description as description',
                 'groupchats.status as status',
-                'states.name as state_id',
-                'countries.name as country_id',
-                'regions.name as region_id',
-                'districts.name as district_id',
-                'zones.name as zone_id',
-                'areas.name as area_id',
-                'churches.name as church_id',
+                // 'states.name as state_id',
+                // 'countries.name as country_id',
+                // 'regions.name as region_id',
+                // 'districts.name as district_id',
+                // 'zones.name as zone_id',
+                // 'areas.name as area_id',
+                // 'churches.name as church_id',
             ]);
 
         return $query;
